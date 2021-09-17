069706e1cb8e23d468ed15f85c75014bcc6b319b
Aleks <aleksandar.trpkovski@gmail.com>
2021-09-17 21:53:02 +1000

    added this project to github

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref, onErrorCaptured } from "vue";
+import Result from "./components/Result.vue";
+
+const error = ref(null);
+
+onErrorCaptured(() => {
+    error.value = "Ohh! Something went wrong!";
+});
+</script>
+
+<template>
+    <div v-if="error">{{ error }}</div>
+    <Suspense v-else>
+        <template #default>
+            <Result />
+        </template>
+        <template #fallback>
+            <h1>Loading...</h1>
+        </template>
+    </Suspense>
+</template>
+
+<style>
+html,
+body {
+    font-size: 100%;
+    overflow-x: hidden;
+    align-content: center;
+    background-color: #efe8df;
+    color: #173353;
+    display: grid;
+    justify-content: center;
+    min-height: 100vh;
+}
+
+*,
+*::before,
+*::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    line-height: 1.3;
+}
+
+a {
+    text-decoration: none;
+}
+</style>
---
+++ src/components/Result.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+
+const response = await fetch("http://openlibrary.org/search.json?q=the+lord+of+the+rings");
+const data = await response.json();
+
+const msg = ref(`It has been found: ${data.num_found} results!`);
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+<style scoped></style>
